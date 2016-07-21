1ca1c8881be1805bd3ce93753c8481c462b14a29
XLuoChen <1425145741@qq.com>
2016-07-21 13:49:47 +0800

    refactor

--- main/main.js
+++ main/main.js
@@ -1,41 +1,12 @@
 'use strict';
 
-function printSum(numbers) {
-  const odds = getOdds(numbers);
-  const squares = buildSquares(odds);
-  const summation = buildSummation(squares);
+function printOddsSum(numbers) {
+  const oddsSquaresSum =
+    numbers.filter(number => number % 2)
+      .map(odd => odd * odd)
+      .reduce((prev, curr) => prev + curr);
 
-  console.log(summation);
+  console.log(oddsSquaresSum);
 }
 
-function getOdds(numbers) {
-  const oddNumbers = [];
-
-  for (const number of numbers) {
-    if (number % 2 === 1) {
-      oddNumbers.push(number);
-    }
-  }
-
-  return oddNumbers;
-}
-
-function buildSquares(odds) {
-  return odds.map(odd => odd * odd);
-}
-
-function buildSummation(squares) {
-  let summation = 0;
-  for(const square of squares){
-    summation += square;
-  }
-
-  return summation;
-}
-
-module.exports = {
-  printSum:printSum,
-  getOdds: getOdds,
-  buildSquares:buildSquares,
-  buildSummation:buildSummation
-};
+module.exports = printOddsSum;
