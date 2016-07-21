f35cc44baf1e931d446aec4c87e1e7f725a30f4b
XLuoChen <1425145741@qq.com>
2016-07-21 13:48:27 +0800

    refactor

--- test/main-spec.js
+++ test/main-spec.js
@@ -1,43 +1,16 @@
 'use strict';
 
-const getSumOfSquare = require('../main/main');
+const printOddsSum = require('../main/main');
 
 describe('sumOfSqure', () => {
-
-  let numbers;
-
-  beforeEach(() => {
-    numbers = [1, 2, 3, 4, 5];
-  });
-
-  it('should get odd numbers', () => {
-
-    const odds = getSumOfSquare.getOdds(numbers);
-
-    expect(odds).toEqual([1, 3, 5]);
-  });
-
-  it('should build square', () => {
-
-    const squares = getSumOfSquare.buildSquares([1, 3, 5]);
-
-    expect(squares).toEqual([1,9,25]);
-  });
-
-  it('should build summation', () => {
-
-    const summations = getSumOfSquare.buildSummation([1, 9, 25]);
-
-    expect(summations).toEqual(35);
-  });
 
   it('should print sum of squares', () => {
 
     spyOn(console, 'log');
 
-    const expectSummation = 35;
+    printOddsSum([1, 2, 3, 4, 5]);
 
-    getSumOfSquare.printSum(numbers);
+    const expectSummation = 35;
 
     expect(console.log).toHaveBeenCalledWith(expectSummation);
   })
